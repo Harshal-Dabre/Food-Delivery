6cec31911cb398cc8986fe26d2b976755a8d87fc
Harshal-Dabre <141765092+Harshal-Dabre@users.noreply.github.com>
2025-05-06 10:07:46 +0530

    changes made

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import '../App.css';
 import Restaurantcard from "./Restaurantcard";
 import {  ResList } from "./config";
+import { FaSearch } from 'react-icons/fa';
 
 
 function filterData(searchTxt,restaurants){
@@ -11,36 +12,56 @@ function filterData(searchTxt,restaurants){
 
 }
 
+function matchResult(item, searchTxt){
+  if(searchTxt)
+    return item?.data?.name?.toLowerCase().includes(searchTxt.toLowerCase())
+  return false
+}
+
+
 const Body = () => {
 
   
-  const [restaurants, setRestaurants] = useState(ResList)
+  const [restaurants, setRestaurants] = useState(ResList) ;
   const [searchTxt, setSearchTxt] = useState("") ;
+  const [suggestionList, setSuggestionList] = useState(ResList);
+ 
 
   return(
     <>
     <div className="body">
        
-       <div className="search-container">
+        <div className="search-container">
+           <div className="search-bar">
             <input type="text"
-            className="search-input"
-            placeholder="search"
-            value={searchTxt}
-            onChange={(e) => {
-              setSearchTxt(e.target.value) ;
-                
-            }} />
+              className="search-input"
+              placeholder="Search for restaurant and food"
+              value={searchTxt}
+              onChange={(e) => {
+                setSearchTxt(e.target.value) ;
+                  
+              }} />
            
-           <button className="search-btn" onClick={()=>{
-              const data = filterData(searchTxt, restaurants)
-              setRestaurants(data) ;
-            }}>Search </button>
+            <button className="search-btn" onClick={()=>{
+                const data = filterData(searchTxt, restaurants)
+                setRestaurants(data) ;
+              }}>
+               <FaSearch className="search-icon" />
+            </button>
+           </div>
+           <div className="suggestion-box">
+            {
+              suggestionList.filter((item) => matchResult(item, searchTxt)).map((item) => (
+                <div className="suggestionList" key={item?.data?.id} >
+                  {item?.data?.name}
+                </div>
+              ))
+              
+            }
+          </div> 
 
-            
-
-          </div>
-       
-         
+  
+        </div> 
         <div className="res-container">
         {
          restaurants.map((restaurant) => {
